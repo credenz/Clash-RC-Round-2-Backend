778faeb452a2c751c27d7cc689370dac7ec93f89
koushal2001 <bhatkoushal2001@gmail.com>
2020-08-21 09:08:14 +0530

    Timer Function

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+import datetime
+import os
+import re
 
-# Create your views here.
+starttime = 0
+endtime = 0
+totaltime = 0
+start = datetime.datetime(2020, 1, 1, 0, 0)
+flag = False
+
+
+def timer(request):
+    if request.method == 'GET':
+        return render(request, 'Users/timer.html')
+
+    elif request.method == 'POST':
+        global starttime, start
+        global endtime
+        global totaltime
+        global flag
+        flag = True
+        request.POST.get('totaltime')
+        start = datetime.datetime.now()
+        time = start.second + start.minute * 60 + start.hour * 60 * 60
+        starttime = time
+        endtime = time + int(totaltime)
+        return HttpResponse(" timer is set ")
