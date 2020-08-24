f96a444c35d7d735ac372ff9931bb642076a712a
sammmeeerrrxxx <memonsameer4@gmail.com>
2020-08-24 18:59:59 +0530

    Updated models blueprint. Tentatively final.

--- ClashRCRound2/settings.py
+++ ClashRCRound2/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Users',
+    'Users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- Users/models.py
+++ Users/models.py
@@ -4,21 +4,21 @@ from django.core.files.storage import FileSystemStorage
 import os
 
 # Create your models here.
-class UserInfo(models.Model):
+class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	totalScore = models.IntegerField(default=0)
 	name = models.CharField(max_length=100)
-	#email = models.EmailField(default='example@gmail.com')
+	email = models.EmailField(default='example@gmail.com')
 	#phone = models.CharField(max_length=10)
 	#timer = models.TimeField(default='00:00') # about this not sure will be required or not.
-	def_lang = models.CharField(max_length=5, default='cpp')
-	cur_lang = models.CharField(max_length=3, default='cpp') # maybe if he chooses to submit current question in any other lang
-
+	#def_lang = models.CharField(max_length=5, default='cpp')
+	#cur_lang = models.CharField(max_length=3, default='cpp') # maybe if he chooses to submit current question in any other lang
+	college = models.CharField(blank=True, max_length=255)
 
     def __str__(self):
 		return self.user.username
 
-class Question(models.Model):
+'''class Question(models.Model):
 	#fs = FileSystemStorage(location = './Questions')
 	#fs2 = FileSystemStorage(location = './Questions/Input')
 	#fs3 = FileSystemStorage(location = './Questions/Output')
@@ -46,7 +46,7 @@ class TestCase(models.Model):
 
 
 
-'''class Responses(models.Model):
+class Responses(models.Model):
 	#def __init__(self):
 		#check file extension and send to respective compiler
 		#also add validation code here
@@ -63,5 +63,22 @@ class TestCase(models.Model):
 	response = models.FileField(storage = fs4)
 	questions = models.ForiegnKey(Questions, on_delete = models.CASCADE)'''
 
+class Question(models.Model):
+    quesTitle = models.CharField(max_length=255)
+    quesDesc = models.TextField()
+    sampleInput = models.TextField()
+    sampleOutput = models.TextField()
 
+
+class Submissions(models.Model):
+    languages = [('c', 'C'), ('cpp', 'C++'), ('py', 'Python')]
+
+    quesID = models.ForeignKey(Question, on_delete=models.CASCADE)
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    codeLang = models.CharField(max_length=3, choices=languages)
+    submission = models.FileField(upload_to='./responses', max_length=100)
+    # submittedCode = models.FilePathField
+    # testResult = models.JSONField() or HStoreField (django.contrib.postgres required) or ArrayField --> TBD
+    submissionTime = models.DateTimeField(auto_now=True)
+    score = models.IntegerField()
 
