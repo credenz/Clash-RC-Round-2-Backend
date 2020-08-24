56da1034688e9d7bd222e4621eb77353f8e36ac4
koushal2001 <bhatkoushal2001@gmail.com>
2020-08-24 20:42:14 +0530

    Leader_Board_Function

--- Users/models.py
+++ Users/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 	#cur_lang = models.CharField(max_length=3, default='cpp') # maybe if he chooses to submit current question in any other lang
 	college = models.CharField(blank=True, max_length=255)
 
-    def __str__(self):
+	def __str__(self):
 		return self.user.username
 
 '''class Question(models.Model):
@@ -30,7 +30,7 @@ class Profile(models.Model):
 		basename = "question_"
 		os.makedirs("{}{}{}".format(path, basename, self.id))
 
-    questiontitle = models.CharField(max_length = 20)
+	questiontitle = models.CharField(max_length = 20)
 
 	def __str__(self):
 		return self.questiontitle
@@ -40,7 +40,7 @@ class TestCase(models.Model):
 	timerequired = models.IntegerField()
 	spacerequired = models.IntegerField()
 
-    
+	
 
 
 
@@ -64,21 +64,22 @@ class Responses(models.Model):
 	questions = models.ForiegnKey(Questions, on_delete = models.CASCADE)'''
 
 class Question(models.Model):
-    quesTitle = models.CharField(max_length=255)
-    quesDesc = models.TextField()
-    sampleInput = models.TextField()
-    sampleOutput = models.TextField()
+	quesTitle = models.CharField(max_length=255)
+	quesDesc = models.TextField()
+	sampleInput = models.TextField()
+	sampleOutput = models.TextField()
 
 
 class Submissions(models.Model):
-    languages = [('c', 'C'), ('cpp', 'C++'), ('py', 'Python')]
+	languages = [('c', 'C'), ('cpp', 'C++'), ('py', 'Python')]
 
-    quesID = models.ForeignKey(Question, on_delete=models.CASCADE)
-    userID = models.ForeignKey(User, on_delete=models.CASCADE)
-    codeLang = models.CharField(max_length=3, choices=languages)
-    submission = models.FileField(upload_to='./responses', max_length=100)
-    # submittedCode = models.FilePathField
-    # testResult = models.JSONField() or HStoreField (django.contrib.postgres required) or ArrayField --> TBD
-    submissionTime = models.DateTimeField(auto_now=True)
-    score = models.IntegerField()
+	quesID = models.ForeignKey(Question, on_delete=models.CASCADE) #as we are going to keep it as 1,2,3,4,5,6
+	userID = models.ForeignKey(User, on_delete=models.CASCADE)
+	codeLang = models.CharField(max_length=3, choices=languages)
+	submission = models.FileField(upload_to='./responses', max_length=100)
+	# submittedCode = models.FilePathField
+	# testResult = models.JSONField() or HStoreField (django.contrib.postgres required) or ArrayField --> TBD
+	submissionTime = models.DateTimeField(auto_now=True)
+	score = models.IntegerField()
+	latestSubTime = models.TimeField(default='00:00')
 
--- Users/views.py
+++ Users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login, logout, authenticate
+from .models import Profile,Question , Submissions
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime
 from django.contrib.auth.decorators import login_required
-#whenever well write a function which requires the user to be logged in user login_required decorator.
+
+# whenever well write a function which requires the user to be logged in user login_required decorator.
 
 starttime = 0
 endtime = 0
@@ -12,8 +14,8 @@ totaltime = 0
 start = datetime.datetime(2020, 1, 1, 0, 0)
 
 
-#since the timer is required in every page well call this function in every view function
-#ive hashed out the pages bcuz well get the pages from frontend team and well implement on those templates accordingly
+# since the timer is required in every page well call this function in every view function
+# ive hashed out the pages bcuz well get the pages from frontend team and well implement on those templates accordingly
 def Timer(request):
     if request.method == 'POST':
         global starttime, start
@@ -25,7 +27,7 @@ def Timer(request):
         starttime = time
         endtime = time + int(totaltime)
         return HttpResponse(" timer is set ")
-    
+
     return render(request, 'Users/home.html')
 
 
@@ -33,12 +35,32 @@ def UserLogin(request):
     if request.method == 'POST':
         username = request.POST.get('#')
         password = request.POST.get('#')
-        user = authenticate(username = username, password = password)
+        user = authenticate(username=username, password=password)
         if user:
-            return render(request, '#')#login page
+            return render(request, '#')  # login page
         else:
             Timer(request)
-            return render(request, 'Users/home.html', context = {'invalid':'Details Entered are Incorrect!'})#samepage
+            return render(request, 'Users/home.html', context={'invalid': 'Details Entered are Incorrect!'})  # samepage
 
     Timer(request)
     return render(request, 'Users/home.html')
+
+
+def leaderboard(request):
+    scoremap = {}
+    for user in Profile.objects.order_by("-totalScore"):
+        qscores = []
+        for n in range(1, 7):
+            try:
+                check = Submissions.objects.get(quesID=n)
+                qscores.append(check.scoreQuestion)
+            except Submissions.DoesNotExist:
+                qscores.append(0)
+        qscores.append(user.totalScore)
+        scoremap[user.user] = qscores
+
+    sorted(qscores.items(), key=lambda items: (items[1][6], Submissions.latestSubTime))
+    #incase we want to check if the latest sub. time is before end time we need to add here something working on it ...
+    return render(request, 'Users/home.html', context={'dict':qscores, 'range': range(1, 7, 1)})
+    # html for leaderboard is to be created
+
