9da4d926c4abd7c9b18d545a2e2beac4f2e02808
Atharva kulkarni <atharvaanantkulkarni@gmail.com>
2020-09-05 21:28:47 +0530

    syntax corrected

--- Users/urls.py
+++ Users/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('showSubmissions', views.showSubmission, name='showSubmissions'),
     path('instruction',views.instruction,name="instruction"),
     path('emergency',views.emergency_login,name="emergency"),
-    path('question/id/',views.question_view,name='question_view')
+    path('question/<id>/',views.question_view,name='question_view'),
 ]
