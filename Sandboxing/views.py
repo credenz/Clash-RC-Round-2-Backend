5acd19fdffe899fcddab3f4bd00143bcfaf34f42
siddharthkoli <siddharthkoli2401@gmail.com>
2020-09-06 18:37:30 +0530

    subprocess tocompile and run

--- Sandboxing/views.py
+++ Sandboxing/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import subprocess
 import os
 
 # Create your views here.
 
 def compileAndRun(request):
-    os.chdir("questions/usersub/newuser/question1")
-    with open("questions/standard/output/question1/output1.txt", "r+") as idealOutput, open('questions/usersub/newuser/question1/output.txt', 'r') as userOutput, open("questions/standard/input/question1/input1.txt", "r") as input, open("questions/usersub/newuser/question1/error.txt", "w") as e, open(""):
+    defaultDir = os.getcwd()
+    os.chdir("questions/") # change dir based on username and question
+    with open("standard/output/question1/output1.txt", "r") as idealOutput, open("usersub/newuser/question1/output.txt", "r+") as userOutput, open("standard/input/question1/input1.txt", "r") as idealInput, open("usersub/newuser/question1/error.txt", "w+") as e:
+        os.chdir("usersub/newuser/question1/")
         a = subprocess.run(["g++", "question1.cpp"], stderr=e)
         if (a.returncode == 0):
-            p = subprocess.run(["./a.out"], stdin=i, stdout=o, stderr=e, shell=True)
-            o.seek(0)
-            o1 = o.readlines()
-            o2 = t.readlines()
+            p = subprocess.run(["./a.out"], stdin=idealInput, stdout=userOutput, stderr=e)
+            userOutput.seek(0)
+            o1 = userOutput.readlines()
+            o2 = idealInput.readlines()
             if (o1 == o2):
-                return render(request, "SUCCESSFUL")
+                os.chdir(defaultDir)
+                return HttpResponse("SUCCESS")
             else:
-                print("F")
-                return render(request, "FAILED")
+                os.chdir(defaultDir)
+                return HttpResponse("FAILED")
+        os.chdir(defaultDir)
+        return HttpResponse("FAILED")
+    return HttpResponse("ERROR")
