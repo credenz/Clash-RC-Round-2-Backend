6719247297714b8e696f46e664d4b078902d65fc
siddharthkoli <siddharthkoli2401@gmail.com>
2020-09-07 16:54:23 +0530

    fixed small bug

--- Sandboxing/views.py
+++ Sandboxing/views.py
@@ -8,14 +8,15 @@ import os
 def compileAndRun(request):
     defaultDir = os.getcwd()
     os.chdir("questions/") # change dir based on username and question
-    with open("standard/output/question1/output1.txt", "r") as idealOutput, open("usersub/newuser/question1/output.txt", "r+") as userOutput, open("standard/input/question1/input1.txt", "r") as idealInput, open("usersub/newuser/question1/error.txt", "w+") as e:
+    with open("standard/output/question1/output1.txt", "r") as idealOutput, open("usersub/newuser/question1/output.txt", "w+") as userOutput, open("questions/standard/input/question1/input1.txt", "r") as idealInput, open("usersub/newuser/question1/error.txt", "w+") as e:
         os.chdir("usersub/newuser/question1/")
-        a = subprocess.run(["g++", "question1.cpp"], stderr=e)
+        a = subprocess.Popen(["g++", "question1.cpp"], stderr=e)
         if (a.returncode == 0):
-            p = subprocess.run(["./a.out"], stdin=idealInput, stdout=userOutput, stderr=e)
+            p = subprocess.Popen(["./a.out"], stdin=idealInput, stdout=userOutput, stderr=e)
+            p.wait()
             userOutput.seek(0)
             o1 = userOutput.readlines()
-            o2 = idealInput.readlines()
+            o2 = idealOutput.readlines()
             if (o1 == o2):
                 os.chdir(defaultDir)
                 return HttpResponse("SUCCESS")
