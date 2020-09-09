1c1bd745791a1a492d2cfd9f2a4408920237f9ea
krishnumkhodke <krishnum.khodke@gmail.com>
2020-09-10 00:58:35 +0530

    user code input

--- Sandboxing/views.py
+++ Sandboxing/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import subprocess
 import os
-#from resource import *
+from resource import *
 import datetime
 from Users import models
 # Create your views here.
