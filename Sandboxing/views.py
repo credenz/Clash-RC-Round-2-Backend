112f925529298c21ca04f3ceab44bd498b031a50
siddharthkoli <siddharthkoli2401@gmail.com>
2020-09-10 17:39:03 +0530

    removed redundancy in file ops, added support for all langs

--- Sandboxing/views.py
+++ Sandboxing/views.py
@@ -28,18 +28,37 @@ def imposeLimits(qno,tc):
     # setrlimit(RLIMIT_RTTIME, (1, 1)) # WILL LIMIT CPU TIME FOR THE PROCESS
 
 
-def compileAndRun(request, qno=None,testcase=None):
+def compileAndRun(request, username, qno=None,testcase=None, lang):
     defaultDir = os.getcwd()
     os.chdir("questions/")
-    with open("standard/output/question{}/output{}.txt".format(qno,testcase), "r") as idealOutput, open("usersub/newuser/question{}/output{}.txt".format(qno,testcase),
-                                                                                 "w+") as userOutput, open(
-            "standard/input/question{}/input{}.txt".format(qno,testcase), "r") as idealInput, open("usersub/newuser/question{}/error{}.txt".format(qno,testcase),
+    with open("standard/output/question{}/output{}.txt".format(qno,testcase), "r") as idealOutput, open("usersub/newuser/question{}/output.txt".format(qno),
+                                                                                 "r+") as userOutput, open(
+            "standard/input/question{}/input{}.txt".format(qno,testcase), "r") as idealInput, open("usersub/newuser/question{}/error.txt".format(qno),
                                                                             "w+") as e:
-        os.chdir("usersub/newuser/question1/")  # CHANGE DIR BASED ON USERNAME AND QUESTION
+        os.chdir("usersub/{}/question{}/".format(username, qno))  # CHANGE DIR BASED ON USERNAME AND QUESTION
+        arg1 = arg2 = arg3 = arg4 = ''
+        if lang == 'c':
+            arg1 = 'gcc'
+            arg2 = 'question{}.c'.format(qno)
+            arg3 = './a.out'
+        elif lang == 'cpp':
+            arg1 = 'g++'
+            arg2 = 'question{}.cpp'.format(qno)
+            arg3 = './a.out'
+        elif lang == 'py':
+            arg3 = 'python3'
+            arg4 = 'question{}.py'.format(qno)
+        compileCode = [arg1, arg2]
+        runCode = [arg3, arg4]
+        compiled = True
         try:
-            a = subprocess.run(["g++", "question1.cpp"], stderr=e)
-            if (a.returncode == 0):
-                p = subprocess.run(["./a.out"], stdin=idealInput, stdout=userOutput, stderr=e,
+            if lang != 'py':
+                a = subprocess.run(compileCode, stderr=e)
+                if a.returncode != 0:
+                    compiled = False
+            if (compiled):
+                userOutput.truncate(0) # EMPTY OUTPUT FILE TO PREVENT UNNECESSARY CONTENT FROM PREVIOUS RUNS.
+                p = subprocess.run(runCode, stdin=idealInput, stdout=userOutput, stderr=e,
                                    preexec_fn=imposeLimits(qno,testcase))
                 # p.wait()
                 userOutput.seek(0)
@@ -55,6 +74,3 @@ def compileAndRun(request, qno=None,testcase=None):
             return HttpResponse("FAILED")
         except:
             return HttpResponse("ERROR")
-
-
-
