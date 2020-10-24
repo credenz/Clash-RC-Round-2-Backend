4a5dd32e25c2c3579c3dc93a0d920c2613ca3fbd
anupam <anupampatil44@gmail.com>
2020-10-24 20:45:31 +0530

    constraints for score

--- questions/usersub/dummy/question0/question11.cpp
+++ questions/usersub/dummy/question0/question11.cpp
@@ -1,1 +1,8 @@
-None
+#include "/home/anupam/clash_test/ClashRCRound2/Sandboxing/include/sandbox.h"
+#include<iostream>
+using namespace std;
+int main()
+{install_filters();
+    cout<<"33\n";
+    return 0;
+}
